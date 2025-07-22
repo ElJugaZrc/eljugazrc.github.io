cbd8a67231f7d2ee65c598dcd9515b6d8826f85a
ElJugaZrc <eljugazrc@gmail.com>
2025-07-22 17:10:31 +0200

    Actualizado a la última versión

--- docs/preview/publicpianos/mapa.js
+++ docs/preview/publicpianos/mapa.js
@@ -13,125 +13,30 @@ const map = L.map('map').setView([37.3703279, -5.9996015], 3);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution:'&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',}).addTo(map); // Esta línea hace que el mapa se muestre
 
-// Constante para que el icono sea gris (0)
-const IconoGris = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
 
-// Constante para que el icono sea verde (1)
-const IconoVerde = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea amarillo (2)
-const iconoAmarillo = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-yellow.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea rojo (3)
-const IconoRojo = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea negroverde (4)
-const IconoNegroVerde = new L.Icon({
-    iconUrl: 'images/marcadores/marker_negroverde.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea negroamarillo (5)
-const IconoNegroAmarillo = new L.Icon({
-    iconUrl: 'images/marcadores/marker_negroamarillo.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea negrojo (6)
-const IconoNegrojo = new L.Icon({
-    iconUrl: 'images/marcadores/marker_negrojo.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea negris (7)
-const IconoNegris = new L.Icon({
-    iconUrl: 'images/marcadores/marker_negris.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Constante para que el icono sea morado (8)
-const IconoMorado = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41]
-});
-
-// Función para los marcadores (hecha por Github copilot) :( me siento decepcionado conmigo mismo pero es que no entendía un carajo de esto xd)
+// Función para los marcadores (hecha por Github copilot las líneas 18 a 24)
 fetch('pianos.json')
     .then(response => response.json())
     .then(pianos => {
         pianos.forEach(piano => {
-            // Selecciona el icono según el estado
-            let icono;
-            switch (piano.estado) {
-                case 0: icono = IconoGris; break;
-                case 1: icono = IconoVerde; break;
-                case 2: icono = iconoAmarillo; break;
-                case 3: icono = IconoRojo; break;
-                case 4: icono = IconoNegroVerde; break;
-                case 5: icono = IconoNegroAmarillo; break;
-                case 6: icono = IconoNegrojo; break;
-                case 7: icono = IconoNegris; break;
-                case 8: icono = IconoMorado; break;
-                default: icono = IconoGris;
-            }
 
             // Crea el marcador
-            L.marker([piano.latitud, piano.longitud], { icon: icono })
+            L.marker([piano.latitud, piano.longitud], { icon: L.icon({
+                iconUrl: `images/marcadores/${piano.estado}.png`,
+                shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+                iconSize: [25, 41],
+                iconAnchor: [12, 41],
+                popupAnchor: [1, -34],
+                shadowSize: [41, 41]
+            }) })
                 .addTo(map)
                 .bindPopup(
-                    `<h2>${piano.lugar}</h2>
+                    `<h2 style="margin-bottom: 10px;">${piano.lugar}</h2>
+                    <h6 style="margin:0px;">ID(${piano.id})</h6>
                     <hr>
                     <img src='images/pianos/${piano.imagen}' width='300px'>
                     <p>${piano.descripcion}</p>
-                    <a href='${piano.link}' target='_blank' rel='noopener noreferrer'>Abrir en google maps</a>`
+                    <table style="width:100%"><tr><td><a href='${piano.link}' target='_blank' rel='noopener noreferrer'>Open in google maps</a></td><td><h5 style="margin: 0px;">Last update: ${piano.comprobacion}</h5></td></tr></table>`
                 );
         });
     })
